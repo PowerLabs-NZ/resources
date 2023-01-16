04e24d4221b81df18794e3153841b526f51ef728
Jack Glasson <jack@jgsolutions.co.nz>
2023-01-16 14:33:43 +1300

    add security repo

--- 3cxlogger/install.sh
+++ 3cxlogger/install.sh
@@ -24,6 +24,7 @@ if [ ! -d "$PROGRAM_DIR" ]; then
 
         #Add repo
         echo "deb http://deb.debian.org/debian buster main" >> /etc/apt/sources.list
+        echo "deb http://security.debian.org/debian-security buster/updates main " >> /etc/apt/sources.list
 
         sudo apt-get update
 
